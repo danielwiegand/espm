1530442e55fec4b9258c23d04ebb654bafce4807
Daniel Wiegand <daniel.a.wiegand@posteo.de>
2021-04-25 09:34:49 +0200

    right align table

--- www/matomo.js
+++ www/matomo.js
@@ -3,7 +3,7 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//172.17.0.3/";
+  var u="//3.121.100.92:8080/";
   _paq.push(['setTrackerUrl', u+'matomo.php']);
   _paq.push(['setSiteId', '1']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
